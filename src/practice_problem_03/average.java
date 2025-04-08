8d0b332ab7ce365df8bdbf49a0e75ebc0ab6efa5
omorsultan <omorsultansust@gmail.com>
2025-04-08 16:07:39 +0600

    3

--- src/practice_problem_03/average.java
+++ src/practice_problem_03/average.java
@@ -1,10 +1,22 @@
 package practice_problem_03;
 
 import java.util.Scanner;
-
-public class average {
-    public static void main(String[] args){
-        int n;
-        Scanner obj =new Scanner(null)
+public class average{
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+  
+    System.out.println("How many numbers will you enter? ");
+    int n = input.nextInt();
+    
+    double[] numbers = new double[n];
+    double sum = 0;
+    for(int i=0;i<n;i++)
+    {
+      System.out.println("Enter "+(i+1)+"th number");
+      numbers[i] = input.nextDouble();
+      sum +=numbers[i];
     }
+    System.out.println("Average is " + sum / n + ".");
+  }
 }
