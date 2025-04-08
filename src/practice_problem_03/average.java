3771c3ee1b60cc9fe99ae2eff6382e541fcacc45
omorsultan <omorsultansust@gmail.com>
2025-04-08 15:23:03 +0600

    omor

--- src/practice_problem_03/average.java
+++ src/practice_problem_03/average.java
@@ -1,4 +1,10 @@
 package practice_problem_03;
 
+import java.util.Scanner;
+
 public class average {
+    public static void main(String[] args){
+        int n;
+        Scanner obj =new Scanner(null)
+    }
 }
