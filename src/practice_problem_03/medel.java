564cb19136a9aa11493e463ab94309beb8e9684c
omorsultan <omorsultansust@gmail.com>
2025-04-09 11:16:34 +0600

    3

---
+++ src/practice_problem_03/medel.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class madel
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("");
+    }
+}
