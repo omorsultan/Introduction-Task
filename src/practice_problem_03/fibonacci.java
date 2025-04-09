83e893d52e787007e79df6954b26f33842a991a1
omorsultan <omorsultansust@gmail.com>
2025-04-09 19:29:18 +0600

    ok

--- src/practice_problem_03/fibonacci.java
+++ src/practice_problem_03/fibonacci.java
@@ -1,4 +1,14 @@
 package practice_problem_03;
 
-public class fibonacci {
+import java.util.Scanner;
+
+public class fibonacci
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter number of terms: ");
+        int n = obj.nextInt();
+        
+    }
 }
--- src/practice_problem_03/oddEvenCount.java
+++ src/practice_problem_03/oddEvenCount.java
@@ -1,4 +1,24 @@
 package practice_problem_03;
 
-public class oddEvenCount {
+import java.util.Scanner;
+
+public class oddEvenCount
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("How many numbers will you enter ?");
+        int n = obj.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter "+ n +" number");
+        int oddCount = 0;
+        int evenCount = 0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = obj.nextInt();
+            if(arr[i]%2==0)evenCount++;
+            else oddCount++;
+        }
+        System.out.println("There are "+evenCount+" even numbers.\n and "+oddCount+" odd numbers.");
+    }
 }
