d5c389c602285077ff4f5caf0e3825738cdafa7d
omorsultan <omorsultansust@gmail.com>
2025-04-09 12:37:36 +0600

    more

--- src/practice_problem_03/greatest.java
+++ src/practice_problem_03/greatest.java
@@ -1,4 +1,27 @@
 package practice_problem_03;
 
-public class greatest {
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.Scanner;
+
+public class greatest
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("How many numbers will you enter ?");
+        int n = obj.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter "+n+ " number:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]= obj.nextInt();
+        }
+        int ans = arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(ans< arr[i])ans =arr[i];
+        }
+        System.out.println("The greatest number is: "+ans);
+    }
 }
--- src/practice_problem_03/reverse.java
+++ src/practice_problem_03/reverse.java
@@ -1,8 +1,29 @@
 package practice_problem_03;
 
 import java.util.*;
-public class Revarse
+public class reverse
 {
-    Scanner obj = new Scanner(System.in);
-    System.out.println("");
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("How many numbers will you enter ?");
+        int n = obj.nextInt();
+
+        System.out.println("Enter "+ n + " number:");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=obj.nextInt();
+        }
+        for(int i=0;i<n/2;i++)
+        {
+            int temp = arr[i];
+            arr[i]=arr[n-i-1];
+            arr[n-i-1]= temp;
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
