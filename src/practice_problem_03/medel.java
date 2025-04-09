1acc7f46a08800bd4136779384589743b0f536a2
omorsultan <omorsultansust@gmail.com>
2025-04-09 11:17:21 +0600

    check

--- src/practice_problem_03/medel.java
+++ src/practice_problem_03/medel.java
@@ -1,10 +1,28 @@
+package practice_problem_03;
+
 import java.util.Scanner;
 
-public class madel
-{
-    public static void main(String[] args)
-    {
+public class medel {
+    public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Enter your maximum grade of the semester :");
+        Float grade = obj.nextFloat();
+        if(grade>=3.5)
+        {
+            System.out.println("Are you completed the semester? ");
+            boolean sem = obj.nextBoolean();
+            if(sem == true )
+            {
+                System.out.println("Give a medel.");
+            }
+            else
+            {
+                System.out.println("Not selected for a medel.");
+            }
+        }
+        else
+        {
+            System.out.println("Not selected for a medel.");
+        }
     }
 }
--- src/practice_problem_03/prime.java
+++ src/practice_problem_03/prime.java
@@ -1,4 +1,24 @@
 package practice_problem_03;
 
-public class prime {
+import java.util.Scanner;
+
+public class prime
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int x = obj.nextInt();
+        boolean check = false;
+        for(int i=2;i*i<=x;i++)
+        {
+            if(x%i==0)
+            {
+                System.out.println("It is not a prime number.");
+                check = true;
+                break;
+            }
+        }
+        if(check== false) System.out.println("The number is a prime number.");
+    }
 }
