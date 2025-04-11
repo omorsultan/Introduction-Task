e7a88910c420bd5246b759db3bf76ccee68549ed
omorsultan <omorsultansust@gmail.com>
2025-04-11 20:05:55 +0600

    fibonacci

--- src/practice_problem_03/fibonacci.java
+++ src/practice_problem_03/fibonacci.java
@@ -9,6 +9,14 @@ public class fibonacci
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter number of terms: ");
         int n = obj.nextInt();
-        
+        int[] arr = new int[n];
+        arr[0]=0;
+        arr[1] = 1;
+        for(int i=0;i<n;i++)
+        {
+            if(i>1)arr[i]=arr[i-1]+arr[i-2];
+            System.out.print(arr[i]+" ");
+
+        }
     }
 }
