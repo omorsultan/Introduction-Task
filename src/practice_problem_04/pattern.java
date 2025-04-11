978bbbfd77f4a9bd5cb8af1d112934eff9a4c348
omorsultan <omorsultansust@gmail.com>
2025-04-11 20:15:31 +0600

    pattern

--- src/practice_problem_04/pattern.java
+++ src/practice_problem_04/pattern.java
@@ -1,4 +1,20 @@
 package practice_problem_04;
 
-public class pattern {
+import java.util.Scanner;
+
+public class pattern
+{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
 }
